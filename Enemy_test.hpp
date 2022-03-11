4cd6f0881f3f516a58efe7f433799d4da5bd2f3e
Logan.M <70237122+BoLTazmania99@users.noreply.github.com>
2022-03-11 07:58:58 -0800

    Create Enemy_test.hpp

---
+++ Enemy_test.hpp
@@ -0,0 +1,53 @@
+#ifndef __ENEMY_TEST_HPP__
+#define __ENEMY_TEST_HPP__
+#include"Enemy.h"
+#include"BossEnemy.h"
+#include"GruntEnemy.h"
+#include"HealthSpecialist.h"
+#include"CritSpecialist.h"
+#include"DamageSpecialist.h"
+#include"DodgeSpecialist.h"
+#include <memory>
+#include <utility>
+
+#include "gtest/gtest.h"
+
+TEST(EnemyTest, EnemySimple){
+    std::shared_ptr<Enemy> test(new Boss(2));
+ 
+        EXPECT_EQ(test->getCriticalHit(), 0 );	
+}
+TEST(EnemyTest, EnemySetter){
+    std::shared_ptr<Enemy> test(new Boss(2));
+	test->setHealth(100);
+
+        EXPECT_EQ(test->getHealth(),100 );
+}
+TEST(EnemyTest, EnemyModify){
+    std::shared_ptr<Enemy> test(new Boss(2));
+       test->setDamage(150);
+       test->setDodgeCounter(5);
+       test->setCriticalHit(50);
+       test->setHealth(100);
+
+	test->modify();
+
+
+        EXPECT_EQ(test->getHealth(),200 );
+EXPECT_EQ(test->getCriticalHit(),100 );
+EXPECT_EQ(test->getDodgeCounter(),10 );
+EXPECT_EQ(test->getDamage(),300 );
+}
+TEST(EnemyTest, EnemyType){
+    std::shared_ptr<Enemy> baddy(new Boss(2));
+        std::shared_ptr<Enemy> test(new CritSpecialist(baddy));
+	test->modify();
+	
+
+        EXPECT_EQ(baddy->getCategoryType(),"BCOE" );
+}
+
+
+
+
+#endif
