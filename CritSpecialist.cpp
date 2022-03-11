fe854e164bc1760d32e6b49a6483f2dd264ed411
Logan.M <70237122+BoLTazmania99@users.noreply.github.com>
2022-03-11 06:45:57 -0800

    Delete CritSpecialist.cpp

--- CritSpecialist.cpp
+++
@@ -1,7 +0,0 @@
-#include "CritSpecialist.h"
-
-    void CritSpecialist::modify(){
-        enemyObject->setCriticalHit(enemyObject->getCriticalHit() * IQMult);
-
-        enemyObject->setCategoryType(type);
-    }
