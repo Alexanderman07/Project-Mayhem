8403f78248ede708e22faa0ff929f23bf900469e
dbirouty <97381279+dbirouty@users.noreply.github.com>
2022-03-10 21:25:04 -0800

    Update ItemBag.cpp

--- ItemBag.cpp
+++ ItemBag.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
 #include <string>
 #include "ItemBag.h"
-<<<<<<< HEAD
-=======
+
 #include "Player.h"
 #include "Category.h"
->>>>>>> b3b6541c4e10bf79f4adbc316b000a096d5ee6f5
+
 using namespace std;
 
 
 ItemBag::ItemBag(){
-<<<<<<< HEAD
   light_heal = 0;
   medium_heal = 0;
   max_heal = 0;
@@ -47,7 +45,6 @@ void ItemBag::use_medium_potion(Category c){
     cout << "No remaining medium potions available!" << endl;
   }
   else if(c.get_health() == c.get_max_health()){
-=======
 	light_heal = 0;
 	medium_heal = 0;
 	max_heal = 0;
@@ -84,12 +81,10 @@ void ItemBag::use_medium_potion(){
     cout << "No remaining medium potions available!" << endl;
   }
   else if(get_health() == max_health){
->>>>>>> b3b6541c4e10bf79f4adbc316b000a096d5ee6f5
     cout << "Player is already at max health!" << endl;
   }
   else{
     medium_heal -= 1;
-<<<<<<< HEAD
     if(c.get_health() + MEDIUM_HEAL_VALUE > c.get_max_health()){
       c.set_health(c.get_max_health());
     }
@@ -105,7 +100,6 @@ void ItemBag::use_max_potion(Category c){
     cout << "No remaining max potions available!" << endl;
   }
   else if(c.get_health() == c.get_max_health()){
-=======
              if(get_health() + MEDIUM_HEAL_VALUE > max_health){
                   category_health = max_health;
              }
@@ -122,12 +116,10 @@ void ItemBag::use_max_potion(){
     cout << "No remaining max potions available!" << endl;
   }
   else if(get_health() == max_health){
->>>>>>> b3b6541c4e10bf79f4adbc316b000a096d5ee6f5
     cout << "Player is already at max health!" << endl;
   }
   else{
     max_heal -= 1;
-<<<<<<< HEAD
     if(c.get_health() + MAX_HEAL_VALUE > c.get_max_health()){
       c.set_health(c.get_max_health());
     }
@@ -139,7 +131,6 @@ void ItemBag::use_max_potion(){
 }
 
 void ItemBag::use_adderall(Category c){
-=======
     if(get_health() + MAX_HEAL_VALUE > max_health){
       category_health = max_health;
     }
@@ -152,13 +143,11 @@ void ItemBag::use_adderall(Category c){
 
 
 void ItemBag::use_adderall(){
->>>>>>> b3b6541c4e10bf79f4adbc316b000a096d5ee6f5
   if(adderall_count == 0){
     cout << "No adderall remaining!" << endl;
   }
   else{
     adderall_count -= 1;
-<<<<<<< HEAD
     if(c.get_criticalhit() == c.get_max_critical_hit()){
       c.set_max_critical_hit(c.get_max_critical_hit() + 1);
       c.set_criticalhit(c.get_criticalhit() + 1);
@@ -171,7 +160,6 @@ void ItemBag::use_adderall(){
 }
 
 void ItemBag::use_strength_enhancementPills(Category c){
-=======
     if(category_criticalhit == max_criticalhit){
       max_criticalhit += 1;
       category_criticalhit += 1;
@@ -185,13 +173,11 @@ void ItemBag::use_strength_enhancementPills(Category c){
 
 
 void ItemBag::use_strength_enhancementPills(){
->>>>>>> b3b6541c4e10bf79f4adbc316b000a096d5ee6f5
   if(strength_enhancementPills == 0){
     cout << "No strength enhancement pills available!" << endl;
   }
   else{
     strength_enhancementPills -= 1;
-<<<<<<< HEAD
     if(c.get_damage() == c.get_max_damage()){
       c.set_max_damage(c.get_max_damage() + 1);
       c.set_damage(c.get_damage() + 1);
@@ -204,7 +190,6 @@ void ItemBag::use_strength_enhancementPills(){
 }
 
 void ItemBag::top_ramen(Category c){
-=======
     if(category_damage == max_damage){
       max_damage += 1;
       category_damage += 1;
@@ -218,12 +203,10 @@ void ItemBag::top_ramen(Category c){
 
 
 void ItemBag::top_ramen(){
->>>>>>> b3b6541c4e10bf79f4adbc316b000a096d5ee6f5
   if(top_ramen_meals == 0){
     cout << "No top ramen meals available!" << endl;
   }
   else{
-<<<<<<< HEAD
     top_ramen_meals -= 1;
       if(c.get_health() == c.get_max_health()){
         c.set_max_health(c.get_max_health() + 1);
@@ -237,7 +220,6 @@ void ItemBag::top_ramen(){
 }
 
 void ItemBag::use_liquid_luck(Category c){
-=======
     top_ramen_meals -= 1
       if(category_health == max_health){
         max_health += 1;
@@ -251,13 +233,11 @@ void ItemBag::use_liquid_luck(Category c){
 }
 
 void ItemBag::use_liquid_luck(){
->>>>>>> b3b6541c4e10bf79f4adbc316b000a096d5ee6f5
   if(liquid_luck_vials == 0){
     cout << "No liquid luck vials available!" << endl;
   }
   else{
     liquid_luck_vials -= 1;
-<<<<<<< HEAD
     if(c.get_dodgeandcounter() == c.get_max_dodgeandcounter()){
       c.set_max_dodgeandcounter(c.get_max_dodgeandcounter() + 1);
       c.set_dodgeandcounter(c.get_dodgeandcounter() + 1);
@@ -317,7 +297,6 @@ void ItemBag::use_liquid_luck(){
     int ItemBag::get_used_textbook_amount(){
       return used_textbook;
     }
-=======
     if(category_dodgeandcounter == max_dodgeandcounter){
       max_dodgeandcounter += 1;
       category_dodgeandcounter += 1;
@@ -329,4 +308,3 @@ void ItemBag::use_liquid_luck(){
   }
 }
 
->>>>>>> b3b6541c4e10bf79f4adbc316b000a096d5ee6f5
