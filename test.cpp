a4facce48f66f7ff4d0ad2e9c6248b05d9499e2a
Logan.M <70237122+BoLTazmania99@users.noreply.github.com>
2022-03-11 07:56:20 -0800

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,17 @@
+#include"Enemy.h"
+#include"BossEnemy.h"
+#include"GruntEnemy.h"
+#include"HealthSpecialist.h"
+#include"CritSpecialist.h"
+#include"DamageSpecialist.h"
+#include"DodgeSpecialist.h"
+
+#include "Enemy_test.hpp"
+
+#include "gtest/gtest.h"
+
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
